a8796db8348654c1578772fff07af40c2508a3c3
Trevor Hartman <viable.hartman@gmail.com>
2022-08-04 13:18:09 -0700

    Java-Lab-002 Completed

--- src/PricelessScript.java
+++ src/PricelessScript.java
@@ -2,25 +2,39 @@ import java.text.NumberFormat;
 
 public class PricelessScript {
     /**
-     * Proof that MasterCard marketing is in your DNA
-     * @param args Command line arguments [The source file path, The target file path, ...]
+     *  Proof that MasterCard marketing is in your DNA
+     *
+     *  @author Trevor Hartman
+     *  @author Paul Constance
+     *
+     *  @since Version 1.0
+     *
+     * Write a brief summary in this section listing the:
+     * Variable name (int) / Its data type (integer variable) / and example values you can assign them. (whole numbers, floating point w/ decimal)
+     *
+     * Next give TWO example variable names and TWO example variable assignments that are WRONG
+     * int & double
+     *
+     * incompatible type casting / double can't go into int becoming less accurate
+     * formatting error / string instead of String
+     *
      **/
     public static void main(String[] args) {
         // String variables, some with format specifiers
         String scriptTemplateLine1 = "%d tickets: %s";
         String scriptTemplateLine2 = "%d hotdogs, %d popcorn, %d sodas: %s";
-        String scriptTemplateLine3 = "1 autographed baseball %s";
-        String scriptTemplateLine4 = "real conversation with 11 year old son: %s";
+        String scriptTemplateLine3 = "2 autographed baseballs %s";
+        String scriptTemplateLine4 = "watching the Giants win: %s";
         String priceless = "priceless";
 
         // integer variable
-        int people = 2;
+        int people = 3;
         // 32 bit floating point variable
         float ticketPrice = 14.0f;  // 32 Bit, but it does exist!
         // double precision floating point variable
         double itemPrice = 9.0;    // Double precision
         // boolean variable
-        boolean trueOrFalse = true;
+        boolean trueOrFalse = false;
 
         NumberFormat formatter = NumberFormat.getCurrencyInstance();
 
@@ -28,11 +42,12 @@ public class PricelessScript {
                 String.format(scriptTemplateLine1, people,
                         formatter.format(people * ticketPrice)));
 
-        System.out.println(
-                String.format(scriptTemplateLine2, people, people, people,
-                        formatter.format(people * itemPrice)));
+        // Example of using printf and platform specific line separator "%n" to
+        // format instead of String.format
+        System.out.printf((scriptTemplateLine2) + "%n", people, people, people,
+                formatter.format(people * itemPrice));
 
-        itemPrice = 45.0;
+        itemPrice = 90.0;
         System.out.println(
                 String.format(scriptTemplateLine3, formatter.format(itemPrice)));
 
